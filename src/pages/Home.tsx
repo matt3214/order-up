c9f1636750afd192f0d0eab069de08f2a97bc117
Matthew Duffy <matthew@Matthews-MacBook-Pro.local>
2019-10-21 14:11:27 -0400

    final commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import { render } from 'react-dom';
+
+const Home: React.FC=()=>{
+    return(
+        <>
+        <h1>Welcome home</h1>
+        </>
+    )
+};
+
+export default Home
