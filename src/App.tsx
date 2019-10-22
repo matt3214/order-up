419b504bf6e3cd024918261ef61abf0e1fce4484
Matthew Duffy <matthew@Matthews-MacBook-Pro.local>
2019-10-22 09:24:24 -0400

    set up basic tab structure for electron react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import { Route,Redirect} from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+import { apps, flash, send } from 'ionicons/icons';
+import AddPage from './pages/Add';
+import ListPage from './pages/List';
+import StatusPage from './pages/Status';
+
 
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/add" component={AddPage} exact={true} />
+          <Route path="/list" component={ListPage} exact={true} />
+          <Route path="/status" component={StatusPage} exact={true} />
+          <Route path="/" render={() => <Redirect to="/list" />} exact={true} />
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/add">
+            <IonIcon icon={flash} />
+            <IonLabel>Add</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/list">
+            <IonIcon icon={apps} />
+            <IonLabel>List</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/status">
+            <IonIcon icon={send} />
+            <IonLabel>Status</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
