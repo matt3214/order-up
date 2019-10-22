d2d38c659e8068c947b02f4ccaa364ddc4e90a0d
Matt <5385818+matt3214@users.noreply.github.com>
2019-10-22 13:17:59 -0400

    updated node integration to be false

--- public/electron.js
+++ public/electron.js
@@ -11,7 +11,7 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
       devTools: isDev
     },
   })
